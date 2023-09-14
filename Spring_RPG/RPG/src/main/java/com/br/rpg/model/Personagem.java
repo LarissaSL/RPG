81ab823734d2582ee9cc462bc6a21fac3e8c3981
SilvanaMenezes <silvanasales.menezes@gmail.com>
2023-09-14 08:37:07 -0300

    V2 - criando a classe Personagem e a interface FabricaPersonagem

---
+++ Spring_RPG/RPG/src/main/java/com/br/rpg/model/Personagem.java
@@ -0,0 +1,79 @@
+package com.br.rpg.model;
+
+public abstract class Personagem {
+    private String nome;
+    private int vida;
+    private int atq;
+    private int def;
+    private Atacar ataque;
+    private Defender defende;
+
+    public Personagem(String nome, int vida, int atq, int def, Atacar ataque, Defender defende) {
+        this.nome = nome;
+        this.vida = vida;
+        this.atq = atq;
+        this.def = def;
+        this.atacar = ataque;
+        this.defender = defende;
+    }
+
+    //pegue esse método no chatgpt para entender melhor como construí-los
+    public void realizarAtaque(Personagem alvo){
+        ataque.executar(this, alvo);
+    }
+    public void realizarDefeza(int dano){
+        defende.executar(this, dano);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    public int getAtq() {
+        return atq;
+    }
+
+    public void setAtq(int atq) {
+        this.atq = atq;
+    }
+
+    public int getDef() {
+        return def;
+    }
+
+    public void setDef(int def) {
+        this.def = def;
+    }
+
+    public Atacar getAtacar() {
+        return atacar;
+    }
+
+    public void setAtacar(Atacar atacar) {
+        this.atacar = atacar;
+    }
+
+    public Defender getDefender() {
+        return defender;
+    }
+
+    public void setDefender(Defender defender) {
+        this.defender = defender;
+    }
+}
+
+
+
+
---
+++ Spring_RPG/RPG/src/main/java/com/br/rpg/services/FabricaPersonagem.java
@@ -0,0 +1,9 @@
+package com.br.rpg.services;
+
+public interface FabricaPersonagem {
+ Guerreiro criarGuerreiro();
+ Mago criarMago();
+ Gladiadora criarGladiadora();
+ Feiticeira criarFeiticeira();
+
+}
