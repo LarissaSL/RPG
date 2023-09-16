e3c5babd151cb7029b886ed1c3bbfcdda63ed446
SilvanaMenezes <silvanasales.menezes@gmail.com>
2023-09-16 16:57:18 -0300

    V4.2 - inclusão do rodadas.html, atualização da fabricaPersonagem e classeMago

--- Spring_RPG/RPG/src/main/java/com/br/rpg/model/Mago.java
+++ Spring_RPG/RPG/src/main/java/com/br/rpg/model/Mago.java
@@ -1,8 +1,11 @@
 package com.br.rpg.model;
 
-public class Mago extends Personagem {
+public abstract class Mago extends Personagem {
     public Mago(String nome, int vida, int atq, int def) {
         super(nome, vida, atq, def);
     }
+    public abstract String classeMalvado();
+
+    public abstract String classeHeroi();
 }
 
--- Spring_RPG/RPG/src/main/java/com/br/rpg/service/FabricaPersonagem.java
+++ Spring_RPG/RPG/src/main/java/com/br/rpg/service/FabricaPersonagem.java
@@ -7,16 +7,9 @@ import com.br.rpg.model.Mago;
 
 
 public interface FabricaPersonagem {
-<<<<<<< HEAD
   Guerreiro criarGuerreiro();
   Mago criarMago();
   Gladiadora criarGladiadora();
   Feiticeira criarFeiticeira();
-=======
- //Guerreiro criarGuerreiro();
- Mago criarMago();
- //Gladiadora criarGladiadora();
- Feiticeira criarFeiticeira();
->>>>>>> 17e33ae15b4332318a1745690d033cec6950b4b8
 
 }
