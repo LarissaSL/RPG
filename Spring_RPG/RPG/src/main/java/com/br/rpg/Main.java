2b438190690b168c6e2cf220cc5066e59891b0ad
SilvanaMenezes <silvanasales.menezes@gmail.com>
2023-10-02 22:50:10 -0300

    V8.1 - InCSS responsivo do footer nas páginas index, inimigos e batalha

--- Spring_RPG/RPG/src/main/java/com/br/rpg/Main.java
+++ Spring_RPG/RPG/src/main/java/com/br/rpg/Main.java
@@ -2,7 +2,7 @@ package com.br.rpg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
+
 
 @SpringBootApplication
 public class Main {
